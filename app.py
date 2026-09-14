ade639c1a06b5a3830d5d95abb42b52185adfac3
Teghen53 <150985434+Teghen53@users.noreply.github.com>
2026-09-14 15:33:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ st.set_page_config(
     page_title="Polymer Flood Screening Tool",
     page_icon="🧪",
     layout="wide",
+    initial_sidebar_state="expanded", # <-- forces sidebar open on load
 )
 
 st.title("🧪 Polymer Flood Screening & Design Tool")
